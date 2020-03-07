db4ee14e6c66b75fa59d088d4952db3d048e128b
kkarpesh <karpeshkir@gmail.com>
2020-03-07 05:41:15 -0500

    deleted unused constants

--- dress-rental-model/src/main/java/com/epam/brest/courses/constants/DressConstants.java
+++ dress-rental-model/src/main/java/com/epam/brest/courses/constants/DressConstants.java
@@ -4,8 +4,5 @@ public class DressConstants {
     public static final String DRESS_ID = "dressId";
     public static final String DRESS_NAME = "dressName";
 
-    public static final String COLUMN_DRESS_ID = "DRESS_ID";
-    public static final String COLUMN_DRESS_NAME = "DRESS_NAME";
-
     public static final int DRESS_NAME_SIZE = 30;
 }
--- dress-rental-model/src/main/java/com/epam/brest/courses/constants/RentConstants.java
+++ dress-rental-model/src/main/java/com/epam/brest/courses/constants/RentConstants.java
@@ -6,12 +6,6 @@ public class RentConstants {
     public final static String RENT_DATE = "rentDate";
     public final static String DRESS_ID = "dressId";
 
-    public final static String COLUMN_RENT_ID = "rent_id";
-    public final static String COLUMN_CLIENT = "client";
-    public final static String COLUMN_RENT_DATE = "rent_date";
-    public final static String COLUMN_DRESS_ID = "dress_id";
-
-
     public final static int RENT_CLIENT_SIZE = 50;
 
 
