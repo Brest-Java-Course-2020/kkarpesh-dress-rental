0090ef6f06c4ab3d59a0b5c6d95b0a150a79c3d6
kkarpesh <karpeshkir@gmail.com>
2020-04-05 19:49:27 -0400

    added Exce[tionControllerAdvice for mvc controllers

--- dress-rental-web-app/src/main/java/com/epam/brest/courses/web_app/exception_handler/ExceptionControllerAdvice.java
+++ dress-rental-web-app/src/main/java/com/epam/brest/courses/web_app/exception_handler/ExceptionControllerAdvice.java
@@ -1,4 +1,58 @@
 package com.epam.brest.courses.web_app.exception_handler;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
+
+/**
+ * Class for handling errors.
+ */
+@ControllerAdvice
 public class ExceptionControllerAdvice {
+
+    /**
+     * Handles http client error exception.
+     *
+     * @param ex HttpClientErrorException.
+     * @param model model for storing information for rendering a view.
+     * @return view name.
+     */
+    @ExceptionHandler(HttpClientErrorException.class)
+    public final String handleClientError(HttpClientErrorException ex,
+                                          Model model) {
+        model.addAttribute("message", ex.getMessage());
+        return "error";
+    }
+
+    /**
+     * Handles http server error exception.
+     *
+     * @param ex HttpServerErrorException.
+     * @param model model for storing information for rendering a view.
+     * @return view name.
+     */
+    @ExceptionHandler(HttpServerErrorException.class)
+    public final String handleServerError(HttpServerErrorException ex,
+                                          Model model) {
+        model.addAttribute("message", ex.getMessage());
+        return "error";
+    }
+
+    /**
+     * Handles resource access exception.
+     *
+     * @param ex ResourceAccessException.
+     * @param model model for storing information for rendering a view.
+     * @return view name.
+     */
+    @ExceptionHandler(ResourceAccessException.class)
+    public final String handleAccessError(ResourceAccessException ex,
+                                          Model model) {
+        model.addAttribute("message",
+                "Oops, something went wrong. Please try again later.");
+        return "error";
+    }
 }
